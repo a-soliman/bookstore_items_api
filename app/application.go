2422dbdfe54cac63a5b22bfccf64cbda35eaff4f
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-07 06:27:22 -0800

    updated the configuration for debugging

--- app/application.go
+++ app/application.go
@@ -20,9 +20,9 @@ func StartApplication() {
 
 	server := &http.Server{
 		Handler:      router,
-		Addr:         "localhost:8000",
-		WriteTimeout: 500 * time.Millisecond,
-		ReadTimeout:  2 * time.Second,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 		IdleTimeout:  60 * time.Second,
 	}
 
